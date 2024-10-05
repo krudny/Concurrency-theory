eb75acd2a1aeeb58924e4aa17ef6458ca86fd2db
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-10-05 19:29:54 +0200

    Added threads

--- Lab1/src/main/java/com/agh/Counter.java
+++ Lab1/src/main/java/com/agh/Counter.java
@@ -1,2 +1,19 @@
-package com.agh;public class Counter {
+package com.agh;
+
+public class Counter {
+    private int cnt = 0;
+
+    public void increment() {
+        this.cnt += 1;
+    }
+
+    public void decrement() {
+        this.cnt -= 1;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+
 }

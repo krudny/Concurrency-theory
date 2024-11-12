1c10ee482f99b5529800612dd551cfd4405adf06
Kamil Rudny <k.rudny1@gmail.com>
2024-11-12 17:32:39 +0100

    Implemented linkedlist methods

--- Lab6/src/main/java/com/agh/List.java
+++ Lab6/src/main/java/com/agh/List.java
@@ -1,24 +1,77 @@
 package com.agh;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class List {
     private ArrayList<ListNode> nodes;
+    private ListNode head;
 
-    public List(int N) {
+    public List() {
         nodes = new ArrayList<>();
-        ListNode first = new ListNode();
-        first.setValue(0);
-        ListNode prev = first;
-        nodes.add(first);
+    }
 
-        for(int i = 1; i < N; i++) {
-            ListNode nextNode = new ListNode();
-            nextNode.setValue(i);
-            prev.setNext(nextNode);
-            prev = nextNode;
-            nodes.add(nextNode);
+    public boolean contains(Object o) {
+        if (head == null) {
+            return false;
         }
+
+        ListNode currNode = head;
+        while (currNode.getNext().isPresent()) {
+            currNode = currNode.getNext().get();
+            if (currNode.getValue() == o) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean remove(Object o) {
+        if(head == null || head.getNext().isEmpty()) {
+            return false;
+        }
+
+        ListNode prevNode = head;
+        ListNode currNode = head.getNext().get();
+        while (currNode.getNext().isPresent()) {
+            if (currNode.getValue() != o) {
+                prevNode = currNode;
+                currNode = currNode.getNext().get();
+            } else {
+                prevNode.setNext(currNode.getNext().get());
+                currNode.setNext(null);
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public boolean add(Object o) {
+        if (head == null) {
+            head = new ListNode(o, null);
+            nodes.add(head);
+            return true;
+        }
+
+        ListNode currNode = head;
+        while (currNode.getNext().isPresent()) {
+            currNode = currNode.getNext().get();
+        }
+        ListNode newNode = new ListNode(o, null);
+        currNode.setNext(newNode);
+        nodes.add(newNode);
+
+        return true;
+    }
+
+    public Optional<ListNode> getHead() {
+        if(nodes.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(nodes.getFirst());
     }
 
     public ArrayList<ListNode> getNodes() {
--- Lab6/src/main/java/com/agh/Main.java
+++ Lab6/src/main/java/com/agh/Main.java
@@ -4,16 +4,27 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int N = 5;
-        List list = new List(5);
+        int N = 10;
+        List list = new List();
+
+        for(int i = 0; i < N; i++) {
+            list.add(i);
+        }
+
+
         ArrayList<ListNode> nodes = list.getNodes();
 
+        list.remove(3);
         ListNode currNode = nodes.getFirst();
-
         while (currNode.getNext().isPresent()) {
-            System.out.println(currNode.getValue());
+            System.out.print(currNode.getValue());
+            System.out.print(currNode.getNext().get().getValue());
+            System.out.println();
+
             currNode = currNode.getNext().get();
+
         }
+
 
     }
 }
